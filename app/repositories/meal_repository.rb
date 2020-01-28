e7da73e886261b3e6934c290702191f79745f44d
Douglas Berkley <douglasmberkley@gmail.com>
2020-01-28 18:32:49 +0900

    added base repo for all repositories

--- app/repositories/meal_repository.rb
+++ app/repositories/meal_repository.rb
@@ -1,44 +1,12 @@
 require 'csv'
 require_relative '../models/meal'
+require_relative 'base_repository'
 
-class MealRepository
-  def initialize(csv_file_path)
-    @csv_file_path = csv_file_path
-    @meals = []
-    @next_id = 1
-    load_csv if File.exist?(@csv_file_path)
-  end
+class MealRepository < BaseRepository
+  def build_element(row)
+    row[:id] = row[:id].to_i
+    row[:price] = row[:price].to_i
 
-  def all
-    @meals
-  end
-
-  def add(meal) # instance
-    meal.id = @next_id
-    @meals << meal
-    @next_id += 1
-    save_csv
-  end
-
-  private
-
-  def load_csv
-    csv_options = {headers: :first_row, header_converters: :symbol }
-    CSV.foreach(@csv_file_path, csv_options) do |row|
-      row[:id] = row[:id].to_i
-      row[:price] = row[:price].to_i
-
-      @meals << Meal.new(row)
-    end
-    @next_id = @meals.empty? ? 1 : @meals.last.id + 1
-  end
-
-  def save_csv
-    CSV.open(@csv_file_path, 'wb') do |csv|
-      csv << ['id', 'name', 'price']
-      @meals.each do |meal|
-        csv << [meal.id, meal.name, meal.price]
-      end
-    end
+    Meal.new(row)
   end
 end
